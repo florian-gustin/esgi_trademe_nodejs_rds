10ddf446ea2d37925d7e7271453bac47df98d1f0
Florian Gustin <florian.gustin.business@gmail.com>
2022-02-06 17:49:37 +0100

    feat: port

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
 const config = {
     db: {
-        // host: process.env.DB_HOST,
-        // port: process.env.DB_PORT,
-        // user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
-        host: "trademe-db.cgck3m3yvpq9.eu-west-1.rds.amazonaws.com",
-        port: 5432,
-        user: "trademe",
-        password: "LyLdveKb0cN7ZCDkfe0k",
-        database: "trademe_db",
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     },
     listPerPage: 10,
 };
