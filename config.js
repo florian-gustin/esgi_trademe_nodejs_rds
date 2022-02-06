331e28ee4b23d08155c15f52a4656c1a77e321c5
Florian Gustin <florian.gustin.business@gmail.com>
2022-02-06 18:01:32 +0100

    fix : err

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
     db: {
-        // host: process.env.DB_HOST,
-        // port: process.env.DB_PORT,
-        // user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     },
     listPerPage: 10,
 };
